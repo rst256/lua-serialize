29d071726977f950a44857b5fdfc4e34c36847f0
rst256 <military@post.cz>
2015-05-31 00:54:22 +0700

    read/write to file test

---
+++ test3.lua
@@ -0,0 +1,31 @@
+local s = require "serialize"
+
+function recursive_compare(t1, t2)
+	if t1==t2 then return true; end
+	if type(t1)~="table" or type(t2)~="table" then return false; end
+	
+	for k1,v1 in pairs(t1) do
+		local v2 = t2[k1];
+		if not recursive_compare(v1,v2) then return false; end
+	end
+	for k2, v2 in pairs(t2) do
+		local v1 = t1[k2];
+		if not recursive_compare(v1,v2) then return false; end
+	end
+	
+	return true;  
+end
+
+addressbook = {
+	name = "Alice",
+	id = 12345,
+	phone = {
+		{ number = "1301234567" },
+		{ number = "87654321", type = "WORK" },
+	}
+}
+
+local addressbook_filename = "addressbook.bin";
+s.fwrite(addressbook, addressbook_filename);
+local addressbook2 = s.fread(addressbook_filename);
+assert(recursive_compare(addressbook, addressbook2));
